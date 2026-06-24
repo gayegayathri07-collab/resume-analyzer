6d5cb7861946b956d054e24077dfbb7d4b286f45
Shafeeka <shafeeka73820@gmail.com>
2026-06-24 03:05:27 -0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ MIDDLEWARE = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DATABASES = {
