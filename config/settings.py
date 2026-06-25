283e086139a1befa90e228725b4fee5128ba1420
Shafeeka <shafeeka73820@gmail.com>
2026-06-24 20:57:48 -0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/db.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
     }
 }
