83d349446f8af5dc3e3cd6c55ed53388ca2a651c
Shafeeka <shafeeka73820@gmail.com>
2026-06-24 21:00:40 -0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -7,24 +7,18 @@ ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.contenttypes',  
-    'django.contrib.auth',          
-    'django.contrib.admin',         
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'app',  # nee app name em unda adi pettu
+    'app',  
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
